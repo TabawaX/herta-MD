95edd7ae254f685f5f1b2d5fe4e667679042bfc8
Tabawa Renki <112997721+TabawaX@users.noreply.github.com>
2024-04-25 19:21:55 +0700

    Create run.js

---
+++ run.js
@@ -0,0 +1,16 @@
+const { spawn } = require('child_process')
+const cp = require('child_process')
+const { promisify } = require('util')
+const exec = promisify(cp.exec).bind(cp)
+
+
+function start(cmd) {
+	return spawn(cmd, [], {
+		stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+	})
+}
+
+start('bash')
+
+console.log('Terminal aman abangkuuuuuhhh')
+￼Enter
